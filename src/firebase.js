6ca61eed80dbf9bb6a1a9f09948119290c07af23
Rafi K Yogananda <rafiyogananda9@gmail.com>
2025-10-25 16:55:13 +0700

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -5,6 +5,18 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCT8Rqh9ZG6rwjbMT3BVVn21vh7rFyTQNA",
+  authDomain: "pyrenees-da24f.firebaseapp.com",
+  projectId: "pyrenees-da24f",
+  storageBucket: "pyrenees-da24f.firebasestorage.app",
+  messagingSenderId: "583225406833",
+  appId: "1:583225406833:web:6e806e7ceb7ae161d372d0",
+  measurementId: "G-X8EGEB08MJ"
+};
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
