07ebd5fbea482a189a3930c204cf35b1b0cb4791
Rafi K Yogananda <rafiyogananda9@gmail.com>
2025-10-25 11:29:21 +0700

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCT8Rqh9ZG6rwjbMT3BVVn21vh7rFyTQNA",
+  authDomain: "pyrenees-da24f.firebaseapp.com",
+  projectId: "pyrenees-da24f",
+  storageBucket: "pyrenees-da24f.firebasestorage.app",
+  messagingSenderId: "583225406833",
+  appId: "1:583225406833:web:97b491348fca81b3d372d0",
+  measurementId: "G-1BXFG8L99C"
+};
